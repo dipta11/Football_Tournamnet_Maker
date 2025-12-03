526e50331dd79c0d3ae8edf4778a18730b73b620
Dipta Bhattacharjee <169031385+dipta11@users.noreply.github.com>
2025-12-03 11:07:44 +0600

    Update Supabase client configuration
    Replaced Supabase URL and anon key with placeholders.

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = "https://lauwsfgxfrjpgnsmfqub.supabase.co"
-const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhdXdzZmd4ZnJqcGduc21mcXViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1MjU2NTQsImV4cCI6MjA3MTEwMTY1NH0.1pvFdwlqEH5w4bK047AI41Ik-9YB-BPKckC1E0FX-YA"
+const supabaseUrl = "your_supabase_url"
+const supabaseAnonKey ="your_supabase_anon_key"
 export const supabase = createClient(supabaseUrl,supabaseAnonKey);
